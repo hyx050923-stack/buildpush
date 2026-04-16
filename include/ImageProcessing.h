bee5666c751fe73d56f1b16c931d7d752f38423f
hyx050923-stack <hyx050923@gmail.com>
2026-04-16 12:11:19 +0000

    4月16日更新

---
+++ include/ImageProcessing.h
@@ -0,0 +1,32 @@
+#ifndef COMBINED_IMAGE_PROC_H
+#define COMBINED_IMAGE_PROC_H
+
+#ifdef _WIN32
+    #ifdef IMAGEPROC_EXPORTS
+        #define IMAGEPROC_API __declspec(dllexport)
+    #else
+        #define IMAGEPROC_API __declspec(dllimport)
+    #endif
+#else
+    #define IMAGEPROC_API __attribute__((visibility("default")))
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+IMAGEPROC_API int runRegionHistogramEqualization(const char* kImagePath);
+
+IMAGEPROC_API void processROIHistEqualization(
+    unsigned char* imgData, int width, int height, 
+    int roiX, int roiY, int roiW, int roiH, 
+    unsigned char* outData);
+
+IMAGEPROC_API bool IntensityNContrast(
+    const char* input_path, const char* output_path, double alpha, double c);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // COMBINED_IMAGE_PROC_H
